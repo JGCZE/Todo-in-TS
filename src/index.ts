3ede45611755522e630e7ec31368d713caba1739
J.Gross <jan.gross.cze@gmail.com>
2023-02-27 13:35:18 +0100

    Added selectors, listener, function for tasks

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
 import { v4 as uuidV4} from "uuid"
-console.log(uuidV4())
+type Task = {
+    id: string,
+    title: string,
+    completed: boolean,
+    createAt: Date    
+}
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form?.addEventListener("submit", e  => {
+    e.preventDefault()
+
+    if(input?.value == "" || input?.value == null) return
+
+    const task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createAt: new Date()
+    }
+
+    addListItem(newTask)
+})
+
+function addListItem(task: Task) {}
